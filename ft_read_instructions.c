0a61bd6b93fc5bc8d825fcf32b8af5becccd471d
akh9001 <akhalidy9001@gmail.com>
2021-09-16 20:03:45 +0100

    checker done & normed, but not tested yet

--- ft_read_instructions.c
+++ ft_read_instructions.c
@@ -6,7 +6,7 @@
 /*   By: akhalidy <akhalidy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/16 11:49:59 by akhalidy          #+#    #+#             */
-/*   Updated: 2021/09/16 19:58:48 by akhalidy         ###   ########.fr       */
+/*   Updated: 2021/09/16 20:03:23 by akhalidy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,27 +23,27 @@ void	ft_exit_checker(char **str, t_dcl **a, t_dcl **b)
 
 void	ft_is_valid_instruction(char **str, t_dcl **a, t_dcl **b)
 {
-	if(!ft_strncmp(*str,"sa", 3))
+	if (!ft_strncmp(*str, "sa", 3))
 		ft_sa(*a);
-	else if(!ft_strncmp(*str,"sb", 3))
+	else if (!ft_strncmp(*str, "sb", 3))
 		ft_sb(*b);
-	else if(!ft_strncmp(*str,"ss", 3))
+	else if (!ft_strncmp(*str, "ss", 3))
 		ft_ss(*a, *b);
-	else if(!ft_strncmp(*str,"pa", 3))
+	else if (!ft_strncmp(*str, "pa", 3))
 		ft_pa(a, b);
-	else if(!ft_strncmp(*str,"pb", 3))
+	else if (!ft_strncmp(*str, "pb", 3))
 		ft_pb(a, b);
-	else if(!ft_strncmp(*str,"ra", 3))
+	else if (!ft_strncmp(*str, "ra", 3))
 		ft_ra(a);
-	else if(!ft_strncmp(*str,"rb", 3))
+	else if (!ft_strncmp(*str, "rb", 3))
 		ft_rb(b);
-	else if(!ft_strncmp(*str,"rr", 3))
+	else if (!ft_strncmp(*str, "rr", 3))
 		ft_rr(a, b);
-	else if(!ft_strncmp(*str,"rra", 4))
+	else if (!ft_strncmp(*str, "rra", 4))
 		ft_rra(a);
-	else if(!ft_strncmp(*str,"rrb", 4))
+	else if (!ft_strncmp(*str, "rrb", 4))
 		ft_rrb(b);
-	else if(!ft_strncmp(*str,"rrr", 4))
+	else if (!ft_strncmp(*str, "rrr", 4))
 		ft_rrr(a, b);
 	else
 		ft_exit_checker(str, a, b);
